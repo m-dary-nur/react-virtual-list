216a7d3633d922748b5855c6cc594d86f7a55ca9
m-dary-nur <m.dary.nur@gmail.com>
2020-05-04 17:05:00 +0700

    add new build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 import React from "react";
-declare const _default: React.MemoExoticComponent<({ items, ...props }: any) => JSX.Element>;
+declare const _default: React.MemoExoticComponent<({ items, wrapperClassName, ...props }: any) => JSX.Element>;
 export default _default;
